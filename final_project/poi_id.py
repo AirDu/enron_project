65ffbd40bff038b07e566e21428eb45ce835b9c0
AirDu <airfreedom5@gmail.com>
2017-10-21 14:08:48 +0800

    Update poi_id.py

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -2,7 +2,10 @@
 # coding=utf-8
 import sys
 import pickle
+import warnings
+warnings.filterwarnings('ignore')
 from pprint import pprint
+from collections import defaultdict
 import numpy as np
 from matplotlib import pyplot
 from sklearn import metrics
@@ -24,6 +27,8 @@ from sklearn.cross_validation import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
 from sklearn.feature_selection import SelectPercentile, f_classif, SelectKBest, chi2, RFE
 
+import tester
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
@@ -32,10 +37,6 @@ features_list = ['poi', 'salary', 'bonus']
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "rb") as data_file:
     data_dict = pickle.load(data_file)
-
-
-def pre_process():
-    pass
 
 
 def choose_best_est(features, labels, scoring):
@@ -70,13 +71,10 @@ def choose_best_est(features, labels, scoring):
             'est': RandomForestClassifier(), 
             'params': {
                 'n_estimators': range(2, 5), 'min_samples_split': range(2, 5), 'max_depth': range(2, 15), 
-                'min_samples_leaf': range(1, 5), 'random_state': [0, 10, 23, 36, 42]
+                'min_samples_leaf': range(1, 5), 'random_state': [0, 10, 23, 36, 42], 'criterion': ['entropy', 'gini']
             }
         }
     ]
-
-    clf = DecisionTreeClassifier(max_depth=8, min_samples_leaf=1)
-    dict_params = {'max_depth': range(5, 15), 'min_samples_leaf': range(1, 5)}
 
     list_grids = []
     for elem in list_elems:
@@ -86,85 +84,14 @@ def choose_best_est(features, labels, scoring):
 
     sorted(list_grids, key=lambda grid: grid.best_score_, reverse=True)
     grid = list_grids[0]
-    # clf = grid.best_estimator_
     for grid in list_grids:
-        print(grid.best_score_)
+        print('[Best Score] - [{0}]'.format(grid.best_score_))
     print('=======================')
     print(grid.best_score_)
     print(grid.best_params_)
     print(grid.best_estimator_)
     print('=======================')
     return grid.best_estimator_
-
-
-def score_by_shuffle(clf, features, labels, n_splits=None):
-    """
-    计算分数，即my_tester.py中的方法
-
-    :param clf: 分类器
-    :param features: 特征集
-    :param labels: 目标集
-    :param n_splits: 分隔数量
-    :return : 分数结果
-    """
-    sss = StratifiedShuffleSplit(n_splits=len(labels), test_size=0.3, random_state=0)
-    true_negatives = 0
-    false_negatives = 0
-    true_positives = 0
-    false_positives = 0
-    for train_idx, test_idx in sss.split(features, labels):
-        features_train = []
-        features_test  = []
-        labels_train   = []
-        labels_test    = []
-        for ii in train_idx:
-            features_train.append( features[ii] )
-            labels_train.append( labels[ii] )
-        for jj in test_idx:
-            features_test.append( features[jj] )
-            labels_test.append( labels[jj] )
-
-        ### fit the classifier using training set, and test on test set
-        clf.fit(features_train, labels_train)
-        predictions = clf.predict(features_test)
-        for prediction, truth in zip(predictions, labels_test):
-            if prediction == 0 and truth == 0:
-                true_negatives += 1
-            elif prediction == 0 and truth == 1:
-                false_negatives += 1
-            elif prediction == 1 and truth == 0:
-                false_positives += 1
-            elif prediction == 1 and truth == 1:
-                true_positives += 1
-            else:
-                print("Warning: Found a predicted label not == 0 or 1.")
-                print("All predictions should take value 0 or 1.")
-                print("Evaluating performance for processed predictions:")
-                break
-    # selector = SelectPercentile(f_classif, percentile=30)
-    # selector.fit(features, labels)
-    try:
-        PERF_FORMAT_STRING = "\
-        \tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
-        Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
-        RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
-        \tFalse negatives: {:4d}\tTrue negatives: {:4d}"
-        total_predictions = true_negatives + false_negatives + false_positives + true_positives
-        accuracy = 1.0*(true_positives + true_negatives)/total_predictions
-        precision = 1.0*true_positives/(true_positives+false_positives)
-        recall = 1.0*true_positives/(true_positives+false_negatives)
-        f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
-        f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
-        print(clf)
-        print(PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5))
-        print(RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives))
-        print("")
-    except Exception as e:
-        print(e)
-        print("Got a divide by zero when trying out:", clf)
-        print("Precision or recall may be undefined due to a lack of true positive predicitons.")
-    result = {'CLF': clf, 'Accuracy': accuracy, 'Precision': precision, 'Recall': recall, 'F1': f1, 'F2': f2}
-    return result
 
 
 def draw_plot(data):
@@ -244,6 +171,28 @@ def gen_features(dataset):
     return set_features
 
 
+def check_nan(my_dataset, n=5):
+    """
+    根据feature为NaN的数量对feature进行排序，
+    移除数据中前n个NaN最多的feature
+
+    :param my_dataset: 数据集
+    :param n: 要移除的前n个NaN最多的特征
+    :return : 移除的特征列表
+    """
+    dict_nan = defaultdict(int)
+    for _, features in my_dataset.items():
+        for feature, value in features.items():
+            if not isinstance(value, int) and not isinstance(value, bool): # and feature not in ['salary', 'bonus']:
+                dict_nan[feature] += 1
+    list_sorted = sorted(dict_nan.items(), key=lambda item: item[1], reverse=True)
+    list_result = [i[0] for i in list_sorted[:n]]
+    for name, _ in my_dataset.items():
+        for feature in list_result:
+            my_dataset[name].pop(feature)
+    return list_result
+
+
 def remove_nan(my_dataset):
     """
     如果bonus或salary为NaN，则移除该员工
@@ -265,7 +214,14 @@ def new_feature(my_dataset):
     :param my_dataset: 数据集
     """
     for name, features in my_dataset.items():
-        my_dataset[name]['bns'] = features['bonus'] + features['salary']
+        if isinstance(features['bonus'], int) and isinstance(features['salary'], int):
+            my_dataset[name]['bns'] = features['bonus'] + features['salary']
+        elif isinstance(features['bonus'], int):
+            my_dataset[name]['bns'] = features['bonus']
+        elif isinstance(features['salary'], int):
+            my_dataset[name]['bns'] = features['salary']
+        else:
+            my_dataset[name]['bns'] = 0
 
 
 def test():
@@ -275,17 +231,15 @@ def test():
     my_dataset = data_dict
     clean_outliers(my_dataset)
 
-    remove_nan(my_dataset)
-    new_feature(my_dataset)
-
-    # print(my_dataset)
+    # new_feature(my_dataset)
 
     features_list = gen_features(my_dataset)
+    list_nan = check_nan(my_dataset)
+    features_list = [f for f in features_list if f not in list_nan]
 
     ### Extract features and labels from dataset for local testing
     data = featureFormat(my_dataset, features_list, sort_keys=True)
     labels, features_rs = targetFeatureSplit(data)
-    # draw_plot(data)
     features_rs = MinMaxScaler().fit_transform(features_rs)
     labels_train, labels_test, features_train, features_test = train_test_split(labels, features_rs, test_size=0.3, random_state=42)
     clf = RandomForestClassifier(max_depth=6, n_estimators=6, min_samples_split=2, min_samples_leaf=2, random_state=36)
@@ -295,47 +249,52 @@ def test():
     print(clf.score(features_test, labels_test))
     print(metrics.precision_score(labels_test, pred))
     print(metrics.recall_score(labels_test, pred))
-    score_by_shuffle(clf, features_rs, labels)
+    # score_by_shuffle(clf, features_rs, labels)
 
 
 def main():
     """
     执行函数
     1.清理异常值
-    2.移除Salary或Bonus为NaN的员工
-    3.增加一个新的特征（bonus + salary）
-    4.生成特征列表
-    5.分离特征和Labels
-    6.选出5个最佳的特征
-    7.依次根据f1, recall, accuracy, precision利用GridSearchCV选出最佳分类器
-    8.用选出的分类器计算分数
+    2.生成特征列表
+    3.移除NaN值最多的N个特征
+    4.分离特征和Labels
+    5.选出5个最佳的特征
+    6.依次根据f1, recall, accuracy, precision利用GridSearchCV选出最佳分类器
+    7.用选出的分类器计算分数
     """
     ### Task 2: Remove outliers
     ### Task 3: Create new feature(s)
     ### Store to my_dataset for easy export below.
     my_dataset = data_dict
     clean_outliers(my_dataset)
-    remove_nan(my_dataset)
-    new_feature(my_dataset)
+    # new_feature(my_dataset)
+
     features_list = gen_features(my_dataset)
+    list_nan = check_nan(my_dataset, n=3)
+    features_list = [f for f in features_list if f not in list_nan]
 
     ### Extract features and labels from dataset for local testing
     data = featureFormat(my_dataset, features_list, sort_keys=True)
     labels, features = targetFeatureSplit(data)
     # draw_plot(data)
     features = MinMaxScaler().fit_transform(features)
-    sb = SelectKBest(chi2, k=5)
+    sb = SelectKBest(chi2, k=3)
     features = sb.fit_transform(features, labels)
 
     list_score = ['f1', 'recall', 'accuracy', 'precision']
-    list_result = []
+    dict_result = {}
     for scoring in list_score:
         print('---====[{0}]====---'.format(scoring))
         clf = choose_best_est(features, labels, scoring)
-        result = score_by_shuffle(clf, features, labels)
-        list_result.append(result)
-        # print('CLF:\n', clf)
-    print(list_result)
+        dict_result[scoring] = clf
+    print('\n>>>>>>>>>>>>>>>>>>>>>>\n')
+    for cate, clf in dict_result.items():
+        print('++++++++++++++++++++++++++')
+        print(cate)
+        print('++++++++++++++++++++++++++')
+        dump_classifier_and_data(clf, my_dataset, features_list)
+        tester.main()
 
     ### Task 4: Try a varity of classifiers
     ### Please name your classifier clf for easy export below.
